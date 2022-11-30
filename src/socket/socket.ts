e67f0367a6ce7ee3480a8d01c66d36f86e0311db
farhajsimform <farhaj.h@simformsolutions.com>
2022-11-30 18:04:23 +0530

    Added socket support between server

---
+++ src/socket/socket.ts
@@ -0,0 +1,14 @@
+import openSocket from 'socket.io-client'
+
+const socket = openSocket(String(process.env.REACT_APP_API_URL), {
+  transports: ['websocket'],
+  rejectUnauthorized: false,
+})
+socket.on("connect", () => {
+    console.log(socket.connected, 'connected'); // true
+  });
+  
+  socket.on("disconnect", () => {
+    console.log(socket.connected, 'disconnected'); // false
+  });
+export { socket }
